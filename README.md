03467756f1aa1558083fa1ea435c1a9215020d1b
Gert-Aesloos <gert.aesloos@student.ucll.be>
2025-05-09 11:17:38 +0200

    pagina word gegenereerd uit json

--- README.md
+++ README.md
@@ -1,5 +1,72 @@
 # ICTProject1_Bas_Dylan_Gert_Robin
 
+## 9/5
+
+website werking veranderd:
+
+- in plaats van 25 dezefde pagina's te maken kan alles nu uit `data.json` gehaald. Data moet hieraan toegevoegd worden.
+
+### Studiekaart
+
+```
+{
+      "id": "",
+      "name": "",
+      "description": "",
+      "features": [
+        "",
+        "",
+        ""
+      ],
+      "color": "#",
+      "colorName": "",
+      "qr": ""
+    },
+```
+
+bij id moet een getal tussen 0 en 24 inc. gezet worden dit verwijst naar het id in de url's en QR codes.
+
+name is de naam van de bachelor
+
+description is de beschrijving over de bachelor
+
+features zijn de 3 kenmerken of vakgebieden
+
+color en colorName zijn de hex waarde en naam van de kleur van de oplossing.
+
+qr krijgt een qr code uit `/images/qr codes`, dit word nog niet toegepast ergens.
+
+### stellingkaart
+
+```
+{
+      "text": "",
+      "studiekaart_id": ""
+    },
+```
+
+text is de stelling die op de kaart staat
+
+studiekaart_id is de id van de studiekaart waar deze bijhoord
+
+### objectkaart
+
+```
+{
+      "image": "",
+      "description": "",
+      "studiekaart_id": ""
+    },
+```
+
+image is een foto of logo van het object/app, deze foto moet in de images folder staan met dezelfde naam
+
+description is de beschrijving van het object/app
+
+studiekaart_id is de id van de studiekaart waar deze bijhoord
+
+nieuwe data mag aan deze json toegevoegd worden.
+
 ## 29/4
 
 kleine aanpassingen gedaan:
---
+++ script.js
@@ -0,0 +1,79 @@
+let cardData = null;
+
+//fetches data from the json
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    cardData = data;
+
+    //gets id from url
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get("id");
+    if (id == null) return;
+
+    //finds studiekaart with matching id
+    const studiekaart = cardData.studiekaarten.find((card) => card.id === id);
+
+    //finds all stellingskaarten from this studiekaart
+    const stellingskaarten = cardData.stellingskaarten.filter(
+      (s) => s.studiekaart_id === id
+    );
+
+    //finds all objectkaarten from this studiekaart
+    const objectkaarten = cardData.objectkaarten.filter(
+      (o) => o.studiekaart_id === id
+    );
+
+    //puts them in list
+    const cards = [...stellingskaarten, ...objectkaarten];
+
+    //fills cards with data
+    for (let card of cards) {
+      populateCard(card);
+    }
+
+    //sets contents of page
+    document.getElementById("name").textContent = studiekaart.name;
+    document.getElementById("description").textContent =
+      studiekaart.description;
+    for (let feature of studiekaart.features) {
+      const li = document.createElement("li");
+      li.textContent = feature;
+      document.getElementById("features").appendChild(li);
+    }
+
+    //sets color of studiekaart as solution
+    const span = document.getElementById("solution");
+    span.style.color = studiekaart.color;
+    span.textContent = studiekaart.colorName;
+  })
+  .catch((error) => console.error("Error loading JSON:", error));
+
+const populateCard = (carddata) => {
+  //creates card, front & back
+  const card = document.createElement("div");
+  card.className = "card";
+
+  const front = document.createElement("div");
+  front.className = "front face";
+
+  const back = document.createElement("div");
+  back.className = "back face";
+
+  //if card has text it is stellingskaart
+  if (carddata.text) {
+    back.innerHTML = carddata.text;
+  }
+  //otherwise it's an objectkaart, has extra image
+  else {
+    back.innerHTML = carddata.description;
+    const img = document.createElement("img");
+    img.src = "https://placecats.com/g/200/100";
+    back.appendChild(img);
+  }
+
+  //adds card to card-container
+  card.appendChild(front);
+  card.appendChild(back);
+  document.getElementById("cards-container").appendChild(card);
+};
